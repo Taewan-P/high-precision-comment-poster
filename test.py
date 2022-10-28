bb0cf3ee215f4557382b34f89f0f21e4ad88c8c0
Taewan Park <swimingtw@gmail.com>
2022-10-28 16:26:01 +0900

    Add cheering test code

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from cheer import get_cheer_id
+from cheer import get_cheer_id, cheer_fan
 from post import get_grp_id
 
 print("--- Test Start ---")
@@ -6,9 +6,11 @@ cafe_link = "https://cafe.daum.net/LeeChaeYeon"
 
 cafe_id = get_grp_id(cafe_link)
 cheer_id = get_cheer_id(cafe_link)
+cheer_result = cheer_fan(cafe_id, cheer_id, 1)
 
 assert cafe_id == "1ZNFK", f"Wrong Cafe ID! - Expected 1ZNFK, but returned {cafe_id}"
 assert cheer_id == 1971, f"Wrong Cheer ID! - Expected 1971, but returned {cheer_id}"
+assert cheer_result == True, f"Cheer Failed! - Expected True, but returned {cheer_result}"
 
 print("Test 1 cleared")
 
@@ -21,5 +23,7 @@ assert cafe_id == "1ZCQy", f"Wrong Cafe ID! - Expedted 1ZCQy, but returned {cafe
 assert cheer_id == 1806, f"Wrong Cheer ID! - Expected 1806, but returned {cheer_id}"
 
 print("Test 2 cleared")
+
+
 
 print("--- Test Passed ---")
